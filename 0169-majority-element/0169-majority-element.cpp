d313d5da28074b7c32df56fd1ba1df55b2097c7e
Anshika Gupta <shigupta2807@gmail.com>
2025-12-23 21:38:26 +0530

    Time: 0 ms (100%), Space: 28.3 MB (27.91%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,19 @@
+ class Solution { 
+    public:
+    int majorityElement(vector<int>& nums) {
+        int count = 0, result = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            if(count == 0) {
+                result = nums[i];
+                count = 1;
+            }
+            else if(result == nums[i]) {
+                count++;
+            }
+            else {
+                count--;
+            }
+        }
+        return result;
+    }
+};
